db478c36072cf69a2af1a28005a0080184f526ec
Hunter <61375197+JZXHanta@users.noreply.github.com>
2024-11-01 05:26:06 -0400

    Added return value object, and added return value statements to evaluator

--- object/object.go
+++ object/object.go
@@ -10,9 +10,10 @@ type Object interface {
 }
 
 const (
-	INTEGER_OBJ = "INTEGER"
-	BOOLEAN_OBJ = "BOOLEAN"
-	NULL_OBJ    = "NULL"
+	INTEGER_OBJ      = "INTEGER"
+	BOOLEAN_OBJ      = "BOOLEAN"
+	NULL_OBJ         = "NULL"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
 )
 
 type Integer struct {
@@ -24,6 +25,10 @@ type Boolean struct {
 }
 
 type Null struct{}
+
+type ReturnValue struct {
+	Value Object
+}
 
 func (i *Integer) Type() ObjectType {
 	return INTEGER_OBJ
@@ -47,4 +52,12 @@ func (n *Null) Type() ObjectType {
 
 func (n *Null) Inspect() string {
 	return "null"
+}
+
+func (rv *ReturnValue) Type() ObjectType {
+	return RETURN_VALUE_OBJ
+}
+
+func (rv *ReturnValue) Inspect() string {
+	return rv.Value.Inspect()
 }
